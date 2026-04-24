eab76934ad5ca7d66becb95262fdb697ae10124d
ayushmi956 <pavani13846@gmail.com>
2026-04-24 18:01:12 +0530

    updated portfolio

---
+++ src/pages/projects.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <div>
+      <h1>My Projects</h1>
+
+      <ul>
+        <li>
+          <strong>Farm 2D</strong> (Group Project) <br />
+          <a
+            href="https://github.com/YajatKumar2/ImmersiveFarm_Game"
+            target="_blank"
+            rel="noreferrer"
+          >
+            View Code
+          </a>
+        </li>
+
+        <br />
+
+        <li>
+          <strong>Fraud Detection</strong> (ML Engineer) <br />
+          <a
+            href="https://github.com/sreekruthy/fraud_detection"
+            target="_blank"
+            rel="noreferrer"
+          >
+            View Code
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Projects;
